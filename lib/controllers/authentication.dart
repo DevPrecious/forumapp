c3bfed2c68cbc95a4a331975249c10920eb0a193
Precious Oladele <Oladelep77@gmail.com>
2022-11-22 01:56:00 +0100

    [add] registeration api

---
+++ lib/controllers/authentication.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:forumapp/constants/constants.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+class AuthenticationController extends GetxController {
+  final isLoading = false.obs;
+
+  Future register({
+    required String name,
+    required String username,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      isLoading.value = true;
+      var data = {
+        'name': name,
+        'username': username,
+        'email': email,
+        'password': password,
+      };
+
+      var response = await http.post(
+        Uri.parse('${url}register'),
+        headers: {
+          'Accept': 'application/json',
+        },
+        body: data,
+      );
+
+      if (response.statusCode == 201) {
+        isLoading.value = false;
+        print(json.decode(response.body));
+      } else {
+        isLoading.value = false;
+        print(json.decode(response.body));
+      }
+    } catch (e) {
+      isLoading.value = false;
+
+      print(e.toString());
+    }
+  }
+}
--- lib/views/register_page.dart
+++ lib/views/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forumapp/controllers/authentication.dart';
 import 'package:forumapp/views/login_page.dart';
 import 'package:get/get.dart';
 import './widgets/input_widget.dart';
@@ -16,7 +17,8 @@ class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _usernameController = TextEditingController();
-
+  final AuthenticationController _authenticationController =
+      Get.put(AuthenticationController());
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size.width;
@@ -79,13 +81,28 @@ class _RegisterPageState extends State<RegisterPage> {
                     vertical: 15,
                   ),
                 ),
-                onPressed: () {},
-                child: Text(
-                  'Register',
-                  style: GoogleFonts.poppins(
-                    fontSize: size * 0.040,
-                  ),
-                ),
+                onPressed: () async {
+                  await _authenticationController.register(
+                    name: _nameController.text.trim(),
+                    username: _usernameController.text.trim(),
+                    email: _emailController.text.trim(),
+                    password: _passwordController.text.trim(),
+                  );
+                },
+                child: Obx(() {
+                  return _authenticationController.isLoading.value
+                      ? const Center(
+                          child: CircularProgressIndicator(
+                            color: Colors.white,
+                          ),
+                        )
+                      : Text(
+                          'Register',
+                          style: GoogleFonts.poppins(
+                            fontSize: size * 0.040,
+                          ),
+                        );
+                }),
               ),
               const SizedBox(
                 height: 20,
